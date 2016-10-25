6672cf986630aa89be60e711e1af015a81f3afa2
Corentin Marciau <corentin@marciau.fr>
2016-10-25 14:35:06 +0200

    [DEV] Add splitted reducers

--- app/reducers.js
+++ app/reducers.js
@@ -2,41 +2,47 @@
  * TodoList tutorial : Reducers
  */
 
-import { VisibilityFilters } from './actions'
+import { combineReducers } from 'redux'
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions'
 
-const initialState = {
-    visibilityFilter: VisibilityFilters.SHOW_ALL,
-    todos: []
-}
+const SHOW_ALL = VisibilityFilters
 
-function todoApp(state = initialState, action) {
+function visibilityFilter(state = SHOW_ALL, action) {
     switch (action.type) {
-        case SET_VISIBILITY_FILTER:
-            return Object.assign({}, state, {
-                visibilityFilter: action.filter
-            })
-        case ADD_TODO:
-            return Object.assign({}, state, {
-                todos: [
-                    ...state.todos,
-                    {
-                        text: action.text,
-                        completed: false
-                    }
-                ]
-            })
-        case TOGGLE_TODO:
-            return Object.assign({}, state, {
-                todos: state.todos.map((todo, index) => {
-                    if (index === action.index) {
-                        return Object.assign({}, todo, {
-                            completed: !todo.completed
-                        })
-                    }
-                    return todo
-                }
-            })
-        default:
-            return state
+    case SET_VISIBILITY_FILTER:
+        return action.filter
+    default:
+	return state
     }
 }
+
+function todos(state = [], action) {
+    switch (action.type) {
+    case ADD_TODO:
+        return [
+                ...state,
+            {
+                text: action.text,
+                completed: false
+            }
+        ]
+    case TOGGLE_TODO:
+        return state.map((todo, index) => {
+            if (index === action.index) {
+                return Object.assign({}, todo, {
+                    completed: !todo.completed
+                })
+            }
+            return todo
+        })
+    default:
+	return state
+    }
+}
+
+const todoApp = combineReducers({
+    visibilityFilter,
+    todos
+})
+
+export default todoApp
